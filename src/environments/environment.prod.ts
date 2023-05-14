25b494194ef201d56d40d098c0145d06f4b6b9cc
fran_richards <meza801168@siigo.com>
2023-05-14 15:41:59 -0500

    fix: config environment to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBfhCLU83PbkLTl9a2lc6EL1McJC97x05w",
+    authDomain: "webappvuejsatuh.firebaseapp.com",
+    projectId: "webappvuejsatuh",
+    storageBucket: "webappvuejsatuh.appspot.com",
+    messagingSenderId: "695595188146",
+    appId: "1:695595188146:web:0acb9b6dc63e1607ab2eab"
+  }
 };
